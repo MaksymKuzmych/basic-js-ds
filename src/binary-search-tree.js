a1da4efc2c1907fed587cace79ad729c875e4fed
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2022-10-15 12:40:30 +0300

    feat: completed tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,148 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js')
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.head = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.head = addWithin(this.head, data)
+
+    function addWithin(node, value) {
+      if (!node) {
+        return new Node(value)
+      }
+
+      if (node.data === value) {
+        return node
+      }
+
+      if (value < node.data) {
+        node.left = addWithin(node.left, value)
+      } else {
+        node.right = addWithin(node.right, value)
+      }
+
+      return node
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.head, data)
+
+    function searchWithin(node, value) {
+      if (!node) {
+        return false
+      }
+
+      if (node.data === value) {
+        return true
+      }
+
+      return value < node.data ? searchWithin(node.left, value) : searchWithin(node.right, value)
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return searchWithin(this.head, data)
+
+    function searchWithin(node, value) {
+      if (!node) {
+        return null
+      }
+
+      if (node.data === value) {
+        return node
+      }
+
+      return value < node.data ? searchWithin(node.left, value) : searchWithin(node.right, value)
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.head = removeNode(this.head, data)
+
+    function removeNode(node, value) {
+      if (!node) {
+        return null
+      }
+
+      if (value < node.data) {
+        node.left = removeNode(node.left, value)
+        return node
+      } else if (node.data < value) {
+        node.right = removeNode(node.right, value)
+        return node
+      } else {
+        // equal - should remove this item
+        if (!node.left && !node.right) {
+          // put null instead of item
+          return null
+        }
+
+        if (!node.left) {
+          // set right child instead of item
+          node = node.right
+          return node
+        }
+
+        if (!node.right) {
+          // set left child instead of item
+          node = node.left
+          return node
+        }
+
+        // both children exists for this item
+        let minFromRight = node.right
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left
+        }
+        node.data = minFromRight.data
+
+        node.right = removeNode(node.right, minFromRight.data)
+
+        return node
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return
+    }
+
+    let node = this.head
+    while (node.left) {
+      node = node.left
+    }
+
+    return node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return
+    }
+
+    let node = this.head
+    while (node.right) {
+      node = node.right
+    }
+
+    return node.data
   }
 }
 
 module.exports = {
-  BinarySearchTree
-};
+  BinarySearchTree,
+}
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js')
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let arr = []
+  let current = l
+
+  while (current.next) {
+    arr.push(current.value)
+    current = current.next
+    if (current.next == null) {
+      arr.push(current.value)
+    }
+  }
+
+  let filteredArr = arr.filter((el) => el !== k)
+
+  let result = filteredArr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur)
+      node.next = acc
+      return node
+    }
+    return new ListNode(cur)
+  }, null)
+
+  return result
 }
 
 module.exports = {
-  removeKFromList
-};
+  removeKFromList,
+}
